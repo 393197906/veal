4164bf78aa7dc55301a7e12193b28d5358963fe4
mr.xie <393197906@qq.com>
2019-04-09 15:48:09 +0800

    #bug vue doc

--- test/vue.test.js
+++ test/vue.test.js
@@ -1,16 +1,13 @@
 import plugin from "../vue"
 
 import Vue from "vue"
-
+Vue.use(plugin, {mixin: {page: 10}})
+const vue = new Vue()
 test("math", () => {
-    Vue.use(plugin)
-    const vue = new Vue()
     expect(vue.$_math.plus(1, 2)).toBe(3)
 })
 
 test("util", () => {
-    Vue.use(plugin)
-    const vue = new Vue()
     const a = () => {
         return 123
     }
@@ -19,16 +16,8 @@ test("util", () => {
     expect(vue.$_util.isFunction(b)).toBe(false)
 })
 
-test("page", () => {
-    Vue.use(plugin)
-    const vue = new Vue()
-    expect(vue.$data.page).toBe(1)
-    expect(vue.$data.pre_page).toBe(20)
-})
 
 test("mixin", () => {
-    Vue.use(plugin, {mixin: {page: 10}})
-    const vue = new Vue()
     expect(vue.$data.page).toBe(10)
     expect(vue.$data.pre_page).toBe(20)
 })
