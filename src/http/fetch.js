b5c2fe6910463fab30fcc73ba7c47b07b922a064
mr.xie <393197906@qq.com>
2019-04-24 11:03:53 +0800

    #bug 删除默认配置

--- src/http/fetch.js
+++ src/http/fetch.js
@@ -1,15 +1,15 @@
 import {mergeOptions, isPlainObject} from "../core"
 
 const defaultParams = {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
+    // method: 'GET',
+    // headers: {
+    //     'Content-Type': 'application/json'
+    // }
 };
 export default (url, params={}) => {
     if (!isPlainObject(params)) throw new Error(`params except a plain object`)
-    const endParams = mergeOptions(defaultParams, params)
-    return window.fetch(url, endParams)
+    // const endParams = mergeOptions(defaultParams, params)
+    return window.fetch(url, params)
 }
 
 
