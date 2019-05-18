fdb6dd5ad0e7aeee9b73f743b545dd51fab57778
mr.xie <393197906@qq.com>
2019-05-18 18:50:47 +0800

    [publish]

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,38 +1,38 @@
 #!/usr/bin/env node
+//
+// const shell = require('shelljs');
+// const { join } = require('path');
+// const { fork } = require('child_process');
+//
+// if (
+//   shell
+//     .exec('npm config get registry')
+//     .stdout.indexOf('https://registry.npmjs.org/') === -1
+// ) {
+//   console.error(
+//     'Failed: set npm registry to https://registry.npmjs.org/ first',
+//   );
+//   process.exit(1);
+// }
+//
+// const cwd = process.cwd();
+// const ret = shell.exec('npx lerna updated').stdout;
+// const updatedRepos = ret
+//   .split('\n')
+//   .map(line => line.replace('- ', ''))
+//   .filter(line => line !== '');
+// if (updatedRepos.length === 0) {
+//   console.log('No package is updated.');
+//   process.exit(0);
+// }
+//
+// const { code: buildCode } = shell.exec('npm run build');
+// if (buildCode === 1) {
+//   console.error('Failed: npm run build');
+//   process.exit(1);
+// }
 
-const shell = require('shelljs');
-const { join } = require('path');
-const { fork } = require('child_process');
-
-if (
-  shell
-    .exec('npm config get registry')
-    .stdout.indexOf('https://registry.npmjs.org/') === -1
-) {
-  console.error(
-    'Failed: set npm registry to https://registry.npmjs.org/ first',
-  );
-  process.exit(1);
-}
-
-const cwd = process.cwd();
-const ret = shell.exec('npx lerna updated').stdout;
-const updatedRepos = ret
-  .split('\n')
-  .map(line => line.replace('- ', ''))
-  .filter(line => line !== '');
-if (updatedRepos.length === 0) {
-  console.log('No package is updated.');
-  process.exit(0);
-}
-
-const { code: buildCode } = shell.exec('npm run build');
-if (buildCode === 1) {
-  console.error('Failed: npm run build');
-  process.exit(1);
-}
-
-const { code: publishCode } = shell.exec('npx lerna publish -y');
+const { code: publishCode } = shell.exec('npx lerna publish from-package');
 if(publishCode ===1 ){
   console.error('Failed: publish');
   process.exit(1);
