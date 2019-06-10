ede754a4f1b57bbc0eeb91075a904c81eaf85d32
mr.xie <393197906@qq.com>
2019-06-10 14:09:42 +0800

    [doc] last update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,10 @@ module.exports = {
     title: 'aevjs',
     description: 'Just playing around',
     themeConfig:{
-        sidebar: 'auto'
+        sidebar: 'auto',
+        lastUpdated: '最后更新',
+    },
+    markdown:{
+        lineNumbers:true
     }
 }
